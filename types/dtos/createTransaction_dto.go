40a0c9df09050f8db79ae021c9973cb4182346d2
umer-emumba <155541934+umer-emumba@users.noreply.github.com>
2024-01-25 17:03:43 +0500

    added numeric rule to transaction dto

--- types/dtos/createTransaction_dto.go
+++ types/dtos/createTransaction_dto.go
@@ -1,7 +1,7 @@
 package dtos
 
 type CreateTransactionDto struct {
-	Amount            float64 `json:"amount" form:"amount" binding:"required,min=1"`
+	Amount            float64 `json:"amount" form:"amount" binding:"required,numeric,min=1"`
 	TransactionTypeID int     `json:"transaction_type_id" form:"transaction_type_id" binding:"required"`
 	CategoryID        int     `json:"category_id" form:"category_id" binding:"required"`
 	TransactionDate   string  `json:"transaction_date" form:"transaction_date" binding:"required,datetime=2006-01-02T15:04:05"`
